6177644eeba75434a921f59acd787a1f1eb65399
malhar <malharansingkar7057@gmail.com>
2021-08-23 22:50:46 +0530

    .

--- src/contentful.js
+++ src/contentful.js
@@ -1,6 +1,6 @@
-import { createClient } from "contentful";
+const contentful = require("contentful");
 
-export default createClient({
+export default contentful.createClient({
   space: "d1il5b1wxjn7",
   accessToken: "NUt5tQ53vZMxAWSH1ft4admoM1nLZeQyR0JrGC75X5o",
 });
